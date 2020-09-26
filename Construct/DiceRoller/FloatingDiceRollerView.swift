aaa6166d5c83250073319bf026b383d133fa289b
Thomas Visser <thomas.visser@gmail.com>
2020-09-26 14:22:47 +0200

    Make floating dice roller draggable

--- Construct/DiceRoller/FloatingDiceRollerView.swift
+++ Construct/DiceRoller/FloatingDiceRollerView.swift
@@ -11,40 +11,62 @@ import SwiftUI
 import ComposableArchitecture
 
 struct FloatingDiceRollerContainerView: View {
+    static let anchors: [UnitPoint] = [.bottomTrailing, .topLeading, .topTrailing, .bottomLeading]
 
     let store: Store<DiceCalculatorState, DiceCalculatorAction>
 
     @State var anchor: UnitPoint = .bottomTrailing
     @State var panelSize: CGSize = .zero
 
+    @State var dragOffset: CGSize = .zero
+
     var body: some View {
         GeometryReader { containerProxy in
-            VStack {
-                DiceCalculatorView(store: store)
-                Button(action: {
-                    withAnimation {
-                        self.anchor = [.bottomTrailing, .topLeading, .topTrailing, .bottomLeading].randomElement()!
-                    }
-                }) { Text("Move") }
-            }
+            DiceCalculatorView(store: store)
+                .frame(minWidth: 250)
                 .fixedSize()
                 .padding(12)
                 .background(Color(UIColor.systemBackground).cornerRadius(12).shadow(radius: 5))
                 .background(GeometryReader { panelProxy in
                     Color.clear.preference(key: CollectionViewSizeKey<Int>.self, value: [0: panelProxy.size])
                 })
-                .position(panelPosition(containerSize: containerProxy.size))
+                .position(panelPosition(for: anchor, in: containerProxy.size))
+                .offset(dragOffset)
+                .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            withAnimation(.interactiveSpring()) {
+                                dragOffset = value.translation
+                            }
+                        }
+                        .onEnded { value in
+                            withAnimation(.spring()) {
+                                self.dragOffset = .zero
+                                self.anchor = anchorNearest(
+                                    to: panelPosition(for: anchor, in: containerProxy.size) + CGPoint(value.predictedEndTranslation),
+                                    in: containerProxy.size
+                                )
+                            }
+                        }
+                )
         }.onPreferenceChange(CollectionViewSizeKey<Int>.self) {
             self.panelSize = $0[0] ?? .zero
         }
         .padding(12)
     }
 
-    func panelPosition(containerSize: CGSize) -> CGPoint {
+    func panelPosition(for anchor: UnitPoint, in containerSize: CGSize) -> CGPoint {
         CGPoint(
             x: (containerSize.width - panelSize.width) * anchor.x + panelSize.width*0.5,
             y: (containerSize.height - panelSize.height) * anchor.y + panelSize.height*0.5
         )
+    }
+
+    func anchorNearest(to position: CGPoint, in containerSize: CGSize) -> UnitPoint {
+        return Self.anchors
+            .map { ($0, panelPosition(for: $0, in: containerSize).distance(to: position)) }
+            .sorted { $0.1 < $1.1 }
+            .first?.0 ?? .bottomTrailing
     }
 
 }
--- Construct/Foundation/Extensions.swift
+++ Construct/Foundation/Extensions.swift
@@ -343,3 +343,17 @@ extension Text {
         }
     }
 }
+
+extension CGPoint {
+    func distance(to point: CGPoint) -> CGFloat {
+        return sqrt(pow((point.x - x), 2) + pow((point.y - y), 2))
+    }
+
+    static func +(lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+        return CGPoint(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
+    }
+
+    init(_ size: CGSize) {
+        self = CGPoint(x: size.width, y: size.height)
+    }
+}
