3b28dbb30036622be96a03fb3143b39cd4a61fcb
Thomas Visser <thomas.visser@gmail.com>
2021-03-24 20:58:44 +0100

    Slightly simplify the popover, remove partly working appear animation

--- Construct/UI/PopoverHost.swift
+++ Construct/UI/PopoverHost.swift
@@ -26,10 +26,9 @@ extension View {
 // Inspired by https://www.objc.io/blog/2020/04/21/swiftui-alert-with-textfield/
 struct PopoverPresenter<Popover>: UIViewControllerRepresentable where Popover: View {
     @Binding var content: Popover?
-    @State var present = false
 
-    func makeUIViewController(context: UIViewControllerRepresentableContext<PopoverPresenter>) -> UIHostingController<EmptyView> {
-        UIHostingController(rootView: EmptyView())
+    func makeUIViewController(context: UIViewControllerRepresentableContext<PopoverPresenter>) -> UIViewController {
+        UIViewController()
     }
 
     final class Coordinator {
@@ -43,30 +42,21 @@ struct PopoverPresenter<Popover>: UIViewControllerRepresentable where Popover: V
         return Coordinator()
     }
 
-    func updateUIViewController(_ uiViewController: UIHostingController<EmptyView>, context: Context) {
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
         let popoverVC = uiViewController.presentedViewController as? UIHostingController<PopoverWrapper>
         if content != nil {
-            popoverVC?.rootView = PopoverWrapper(popover: $content, present: $present)
+            popoverVC?.rootView = PopoverWrapper(popover: $content)
 
             if popoverVC == nil {
-                let newPopoverVC = UIHostingController(rootView: PopoverWrapper(popover: $content, present: $present))
+                let newPopoverVC = UIHostingController(rootView: PopoverWrapper(popover: $content))
                 newPopoverVC.view.backgroundColor = .clear
                 newPopoverVC.modalPresentationStyle = .overFullScreen
                 context.coordinator.popoverVC = newPopoverVC
                 uiViewController.present(newPopoverVC, animated: false)
-
-                DispatchQueue.main.async {
-                    withAnimation {
-                        present = true
-                    }
-                }
             }
         }
 
         if content == nil && popoverVC != nil && popoverVC == context.coordinator.popoverVC {
-            DispatchQueue.main.async {
-                present = false
-            }
             context.coordinator.popoverVC = nil
             uiViewController.dismiss(animated: false)
         }
@@ -74,34 +64,26 @@ struct PopoverPresenter<Popover>: UIViewControllerRepresentable where Popover: V
 
     struct PopoverWrapper: View {
         @Binding var popover: Popover?
-        @Binding var present: Bool
 
         var body: some View {
             ZStack {
-                if let popover = popover, present {
-                    Color(UIColor.systemGray3).opacity(0.45).edgesIgnoringSafeArea(.all)
-                        .onTapGesture {
-                            $popover.wrappedValue = nil
-                        }
-                        .transition(.opacity)
+                Color(UIColor.systemGray3).opacity(0.45).edgesIgnoringSafeArea(.all)
+                    .onTapGesture {
+                        $popover.wrappedValue = nil
+                    }
+                    .transition(.opacity)
 
-                    popover
-                        .padding(15)
-                        .clipped()
-                        .frame(maxWidth: 500)
-                        .background(
-                             Color(UIColor.systemBackground)
-                                .cornerRadius(8)
-                                .shadow(radius: 5)
-                        )
-                        .padding(20)
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
-                        .transition(
-                            AnyTransition.offset(y: 50)
-                                .combined(with: .scale(scale: 0.9))
-                                .combined(with: AnyTransition.opacity.animation(.easeOut(duration: 0.1)))
-                        )
-                }
+                popover
+                    .padding(15)
+                    .clipped()
+                    .frame(maxWidth: 500)
+                    .background(
+                         Color(UIColor.systemBackground)
+                            .cornerRadius(8)
+                            .shadow(radius: 5)
+                    )
+                    .padding(20)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
             }
             .animation(.spring())
         }
