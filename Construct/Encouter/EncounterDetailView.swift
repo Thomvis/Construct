e09fb5f77a77da3dfe5868efdfb7408b059bb19b
Thomas Visser <thomas.visser@gmail.com>
2021-10-13 22:03:18 +0200

    Remove spacing at the top of the encounter detail view
    This reverts a hack that on an earlier iOS version actually removed
    spacing.

--- Construct/Encouter/EncounterDetailView.swift
+++ Construct/Encouter/EncounterDetailView.swift
@@ -34,7 +34,7 @@ struct EncounterDetailView: View {
                 self.viewStore.send(.selection($0))
             })) {
                 if viewStore.state.shouldShowEncounterDifficulty {
-                    Section(header: Text("").accessibilityHidden(true)) {
+                    Section {
                         SimpleButton(action: {
                             self.viewStore.send(.sheet(.settings))
                         }) {
