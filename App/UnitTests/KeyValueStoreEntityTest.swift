817a25ea573782b3c12224b2079f8db69ebe447e
Thomas Visser <thomas.visser@gmail.com>
2022-12-17 21:34:47 +0100

    Add tests covering entity keys

--- App/UnitTests/KeyValueStoreEntityTest.swift
+++ App/UnitTests/KeyValueStoreEntityTest.swift
@@ -10,8 +10,13 @@ import Foundation
 import XCTest
 @testable import Construct
 import Persistence
+import GameModels
+import Compendium
+import Helpers
 
 class KeyValueStoreEntityTest: XCTestCase {
+    static let id1 = UUID(uuidString: "4E7A5EC2-2C3F-49CB-8B0B-063B32DAB616")!
+    static let id2 = UUID(uuidString: "8B4E835F-A058-4619-92A1-3A83F362BE6A")!
 
     func testEnsureUniquePrefixes() {
         for i1 in KeyValueStoreEntityKeyPrefix.allCases.indices {
@@ -22,6 +27,53 @@ class KeyValueStoreEntityTest: XCTestCase {
                 XCTAssertFalse(p1.rawValue.hasPrefix(p2.rawValue), "\(p2) is a prefix of \(p1)")
             }
         }
+    }
+
+    func testEncounterKey() {
+        let e = Encounter(id: Self.id1, name: "", combatants: [])
+        XCTAssertEqual(e.key, "encounter_\(Self.id1)")
+    }
+
+    func testRunningEncounterKey() {
+        let e = Encounter(id: Self.id1, name: "", combatants: [])
+        let re = RunningEncounter(
+            id: Self.id2.tagged(),
+            base: e,
+            current: e
+        )
+        XCTAssertEqual(re.key, "runningEncounter.encounter_\(Self.id1).\(Self.id2)")
+    }
+
+    func testCompendiumEntryKey() {
+        let monster = CompendiumEntry(Monster(
+            realm: .core,
+            stats: apply(.default) { $0.name = "ABC" },
+            challengeRating: .half
+        ))
+        XCTAssertEqual(monster.key, "compendium::monster::core::ABC")
+
+        let character = CompendiumEntry(Character(
+            id: Self.id1.tagged(),
+            realm: .homebrew,
+            stats: .default
+        ))
+        XCTAssertEqual(character.key, "compendium::character::homebrew::\(Self.id1)")
+    }
+
+    func testCampaignNodeKey() {
+        let rootNode = CampaignNode(id: Self.id1.tagged(), title: "", contents: nil, special: nil)
+        XCTAssertEqual(rootNode.key, "campaignNode.\(Self.id1)") // this is unexpected (should start with cn_)
+
+        let topLevelNode = CampaignNode(id: Self.id1.tagged(), title: "", contents: nil, special: nil, parentKeyPrefix: CampaignNode.root.keyPrefixForChildren)
+        XCTAssertEqual(topLevelNode.key, "cn_\(CampaignNode.root.id.rawValue)/.\(Self.id1)")
+    }
+
+    func testPreferencesKey() {
+        XCTAssertEqual(Preferences().key, "Construct::Preferences")
+    }
+
+    func testDefaultContentVersionsKey() {
+        XCTAssertEqual(DefaultContentVersions.current.key, "Construct::DefaultContentVersions")
     }
 
 }
