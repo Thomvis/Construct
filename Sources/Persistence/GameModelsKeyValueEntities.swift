Thomvis/Construct

commit 330ebb060bc00569c9aedcd8c8ffcbeae062d563

Refactor KeyValueStoreEntity to use typed keys
This replaces stringly-typed keys. The keys are still converted to strings
in various places; that could be improved.

Sources/Persistence/GameModelsKeyValueEntities.swift
@@ -7,85 +7,91 @@
 
 import Foundation
 import GameModels
+import Tagged
 
 extension RunningEncounter: KeyValueStoreEntity {
-    public static var keyPrefix: KeyPrefix = .runningEncounter
+    public static var keyPrefix: String = "running"
 
-    public var key: String {
-        return "\(Self.keyPrefix(for: base))\(id)"
+    public var key: Key {
+        return Self.keyPrefix(for: base) + id.uuidString
     }
 
-    public static func keyPrefix(for encounter: Encounter) -> String {
+    public static func keyPrefix(for encounter: Encounter) -> Key {
         keyPrefix(for: encounter.id)
     }
 
-    public static func keyPrefix(for encounterId: Encounter.Id) -> String {
-        "\(Self.keyPrefix).\(Encounter.key(encounterId))."
+    public static func keyPrefix(for encounterId: Encounter.Id) -> Key {
+        Key(id: Encounter.key(encounterId).rawValue + ".", separator: ".")
     }
 }
 
 extension Encounter: KeyValueStoreEntity {
-    public static let keyPrefix: KeyPrefix = .encounter
+    public static let keyPrefix: String = "encounter"
 
-    public var key: String {
+    public var key: Key {
         Self.key(id)
     }
 
-    public static func key(_ id: Encounter.Id) -> String {
-        return "\(Self.keyPrefix)_\(id)"
+    public static func key(_ id: Encounter.Id) -> Key {
+        Key(id: id.uuidString, separator: "_")
     }
 }
 
 extension CampaignNode: KeyValueStoreEntity {
     public static let root = CampaignNode(id: UUID(uuidString: "990EDB4B-90C7-452A-94AB-3857350B2FA6")!.tagged(), title: "ROOT", contents: nil, special: .root, parentKeyPrefix: nil)
-    public static let scratchPadEncounter = CampaignNode(id: UUID(uuidString: "14A7E9D3-14B8-46DF-A7F2-3B5DCE16EEA5")!.tagged(), title: "Scratch pad", contents: CampaignNode.Contents(key: Encounter.key(Encounter.scratchPadEncounterId), type: .encounter), special: .scratchPadEncounter, parentKeyPrefix: CampaignNode.root.keyPrefixForChildren)
+    public static let scratchPadEncounter = CampaignNode(id: UUID(uuidString: "14A7E9D3-14B8-46DF-A7F2-3B5DCE16EEA5")!.tagged(), title: "Scratch pad", contents: CampaignNode.Contents(key: Encounter.key(Encounter.scratchPadEncounterId).rawValue, type: .encounter), special: .scratchPadEncounter, parentKeyPrefix: CampaignNode.root.keyPrefixForChildren.rawValue)
 
-    public static let keyPrefix: KeyPrefix = .campaignNode
+    public static let keyPrefix: String = "cn_"
 
-    public var key: String {
-        if let parent = parentKeyPrefix {
-            return "\(parent)/.\(id)"
+    public var key: Key {
+        if let parent = parentKeyPrefix.flatMap(Key.init(rawKey:)) {
+            return parent + "/." + id.uuidString
         }
-        return "\(Self.keyPrefix).\(id)"
+        return Key(id: id.uuidString, separator: ".")
     }
 
-    public var keyPrefixForChildren: String {
-        if let parent = parentKeyPrefix {
-            return "\(parent)/\(id)"
+    public var keyPrefixForChildren: Key {
+        if let parent = parentKeyPrefix.flatMap(Key.init(rawKey:)) {
+            return parent + "/" + id.uuidString
         }
-        return "cn_\(id)"
+        return Key(id: id.uuidString)
     }
 
     public var keyPrefixForFetchingDirectChildren: String {
-        return "\(keyPrefixForChildren)/."
+        return "\(keyPrefixForChildren.rawValue)/."
     }
 }
 
+/// Preferences is a singleton entity
 extension Preferences: KeyValueStoreEntity {
-    public static let keyPrefix: KeyPrefix = .preferences
-    public static let key = keyPrefix.rawValue
+    public static let keyPrefix: String = "Construct::Preferences"
+    public static let key: Key = Key(id: "")
 
-    public var key: String {
+    public var key: Key {
         Self.key
     }
 }
 
 extension CompendiumEntry: KeyValueStoreEntity {
     static let keySeparator = CompendiumItemKey.keySeparator
-    public static let keyPrefix: KeyPrefix = .compendiumEntry
+    public static let keyPrefix: String = "compendium"
 
-    public var key: String {
+    public var key: Key {
         return Self.key(for: item.key)
     }
 
     static func keyPrefix(for type: CompendiumItemType? = nil) -> String {
-        return [Self.keyPrefix.rawValue, type?.rawValue]
+        return [Self.keyPrefix, type?.rawValue]
             .compactMap { $0 }
             .joined(separator: Self.keySeparator)
     }
 
-    static func key(for itemKey: CompendiumItemKey) -> String {
-        [Self.keyPrefix.rawValue, itemKey.type.rawValue, itemKey.realm.description, itemKey.identifier]
+    static func key(for itemKey: CompendiumItemKey) -> Key {
+        let id = [itemKey.type.rawValue, itemKey.realm.description, itemKey.identifier]
             .joined(separator: Self.keySeparator)
+        return Key(
+            id: id,
+            separator: Self.keySeparator
+        )
     }
 }

App/App/App/EntityChangeObserver.swift
@@ -11,11 +11,11 @@ import ComposableArchitecture
 import Persistence
 
 protocol HavingEntities {
-    var entities: [AnyKeyValueStoreEntity] { get }
+    var entities: [any KeyValueStoreEntity] { get }
 }
 
 extension AppState: HavingEntities {
-    var entities: [AnyKeyValueStoreEntity] {
+    var entities: [any KeyValueStoreEntity] {
         return [
             navigation?.tabState?.entities,
             navigation?.columnState?.entities
@@ -24,25 +24,25 @@ extension AppState: HavingEntities {
 }
 
 extension TabNavigationViewState: HavingEntities {
-    var entities: [AnyKeyValueStoreEntity] {
+    var entities: [any KeyValueStoreEntity] {
         return campaignBrowser.entities
     }
 }
 
 extension ColumnNavigationViewState: HavingEntities {
-    var entities: [AnyKeyValueStoreEntity] {
+    var entities: [any KeyValueStoreEntity] {
         return (campaignBrowse.nextScreen?.entities ?? [])
     }
 }
 
 extension CampaignBrowseViewState: HavingEntities {
-    var entities: [AnyKeyValueStoreEntity] {
+    var entities: [any KeyValueStoreEntity] {
         return (nextScreen?.entities ?? []) + (detailScreen?.entities ?? [])
     }
 }
 
 extension CampaignBrowseViewState.NextScreen: HavingEntities {
-    var entities: [AnyKeyValueStoreEntity] {
+    var entities: [any KeyValueStoreEntity] {
         switch self {
         case .campaignBrowse(let state): return state.entities
         case .encounter(let state): return state.entities
@@ -51,8 +51,8 @@ extension CampaignBrowseViewState.NextScreen: HavingEntities {
 }
 
 extension EncounterDetailViewState: HavingEntities {
-    var entities: [AnyKeyValueStoreEntity] {
-        return [AnyKeyValueStoreEntity(building), AnyKeyValueStoreEntity(running)].compactMap { $0 }
+    var entities: [any KeyValueStoreEntity] {
+        return [building, running.map { $0 as any KeyValueStoreEntity }].compactMap { $0 }
     }
 }
 
@@ -63,11 +63,12 @@ extension KeyValueStore {
     // fixme: naive implementation, might become very slow
     func entityChangeObserver<Environment, State, Action>(initialState: State, reducer: Reducer<State, Action, Environment>) -> Reducer<State, Action, Environment> where State: HavingEntities {
 
+
         var cache: [String: Data] = [:]
 
         for e in initialState.entities {
             if let encoded = try? Self.encoder.encode(e) {
-                cache[e.key] = encoded
+                cache[e.rawKey] = encoded
             }
         }
 
@@ -76,13 +77,13 @@ extension KeyValueStore {
             let effects = reducer.callAsFunction(&state, action, environment)
 
             // fixme: use effects for saving?
-            state.entities.compactMap { e -> (AnyKeyValueStoreEntity, Data)? in
+            state.entities.compactMap { e -> (any KeyValueStoreEntity, Data)? in
                 guard let encoded = try? Self.encoder.encode(e) else { return nil }
 
-                if cache[e.key] == nil {
+                if cache[e.rawKey] == nil {
                     // new entity
                     return (e, encoded)
-                } else if let encoded = try? Self.encoder.encode(e), encoded != cache[e.key] {
+                } else if let encoded = try? Self.encoder.encode(e), encoded != cache[e.rawKey] {
                     // entity changed
                     return (e, encoded)
                 }
@@ -90,9 +91,9 @@ extension KeyValueStore {
             }.forEach { entityAndData in
                 do {
                     try self.put(entityAndData.0)
-                    cache[entityAndData.0.key] = entityAndData.1
+                    cache[entityAndData.0.rawKey] = entityAndData.1
                 } catch {
-                    print("Failed saving changed entity with key \(entityAndData.0.key). Error: \(error)")
+                    print("Failed saving changed entity with key \(entityAndData.0.rawKey). Error: \(error)")
                 }
             }
             return effects

App/App/Campaign/CampaignBrowser.swift
@@ -46,7 +46,7 @@ class CampaignBrowser {
 
     func move(_ node: CampaignNode, to destination: CampaignNode) throws {
         var newNode = node
-        newNode.parentKeyPrefix = destination.keyPrefixForChildren
+        newNode.parentKeyPrefix = destination.keyPrefixForChildren.rawValue
 
         try store.put(newNode)
         try store.remove(node.key)
@@ -54,6 +54,6 @@ class CampaignBrowser {
 
     /// Returns the total number of nodes
     func nodeCount() throws -> Int {
-        return try store.count(CampaignNode.keyPrefix.rawValue)
+        return try store.count(CampaignNode.keyPrefix)
     }
 }

App/App/Campaign/View/CampaignBrowseViewState.swift
@@ -209,7 +209,7 @@ extension CampaignBrowseViewState {
 
     var isMoveOrigin: Bool {
         guard case .move(let nodes) = mode, let node = nodes.first else { return false }
-        return node.parentKeyPrefix == self.node.keyPrefixForChildren
+        return node.parentKeyPrefix == self.node.keyPrefixForChildren.rawValue
     }
 
     func isBeingMoved(_ node: CampaignNode) -> Bool {
@@ -292,7 +292,7 @@ extension CampaignBrowseViewState {
                                 env.crashReporter.trackError(.init(error: error, properties: [:], attachments: [:]))
                             }
 
-                            contents = CampaignNode.Contents(key: encounter.key, type: .encounter)
+                            contents = CampaignNode.Contents(key: encounter.key.rawValue, type: .encounter)
                         }
 
                         do {
@@ -301,7 +301,7 @@ extension CampaignBrowseViewState {
                                 title: title,
                                 contents: contents,
                                 special: nil,
-                                parentKeyPrefix: node.keyPrefixForChildren
+                                parentKeyPrefix: node.keyPrefixForChildren.rawValue
                             ))
                         } catch {
                             env.crashReporter.trackError(.init(error: error, properties: [:], attachments: [:]))

App/App/Compendium/View/CompendiumIndexState.swift
@@ -25,7 +25,7 @@ struct CompendiumIndexState: NavigationStackSourceState, Equatable {
     var results: RS
     var suggestions: [CompendiumEntry]?
     
-    var scrollTo: String? // the key of the entry to scroll to
+    var scrollTo: CompendiumEntry.Key? // the key of the entry to scroll to
 
     var presentedScreens: [NavigationDestination: NextScreen]
     var alert: AlertState<CompendiumIndexAction>?
@@ -273,7 +273,7 @@ extension CompendiumIndexState: NavigationStackItemState {
 enum CompendiumIndexAction: NavigationStackSourceAction, Equatable {
 
     case results(CompendiumIndexState.RS.Action<CompendiumIndexQueryAction>)
-    case scrollTo(String?)
+    case scrollTo(CompendiumEntry.Key?)
     case onQueryTypeFilterDidChange([CompendiumItemType]?, debounce: Bool)
     case onAddButtonTap(CompendiumItemType)
     case onSearchOnWebButtonTap

App/App/Compendium/View/CompendiumIndexView.swift
@@ -386,7 +386,7 @@ fileprivate struct CompendiumItemList: View, Equatable {
         // not used by the view (store is used directly) but here to ensure the view is re-evaluated
         let presentedItemDetail: String?
 
-        let scrollTo: String?
+        let scrollTo: CompendiumEntry.Key?
 
         init(_ state: CompendiumIndexState) {
             self.title = state.title

App/App/Compendium/View/ItemDetailView/CompendiumEntryDetailViewState.swift
@@ -211,7 +211,7 @@ struct CompendiumEntryDetailViewState: NavigationStackSourceState, Equatable {
                 case .sheet(.creatureEdit(CreatureEditViewAction.onRemoveTap)):
                     let entryKey = state.entry.key
                     return Effect.run { subscriber in
-                        _ = try? env.database.keyValueStore.remove(entryKey)
+                        _ = try? env.database.keyValueStore.remove(entryKey.rawValue)
                         subscriber.send(.setSheet(nil))
                         DispatchQueue.main.asyncAfter(deadline: .now()+0.1) {
                             subscriber.send(.didRemoveItem)

App/App/Encouter/EncounterDetailViewState.swift
@@ -316,7 +316,7 @@ extension EncounterDetailViewState {
                     let runningEncounterPrefix = RunningEncounter.keyPrefix(for: state.building)
                     return Effect.future { callback in
                         // remove all runs
-                        _ = try? env.database.keyValueStore.removeAll(runningEncounterPrefix)
+                        _ = try? env.database.keyValueStore.removeAll(runningEncounterPrefix.rawValue)
                         callback(.success(.resumableRunningEncounters(.startLoading)))
                     }
                 case .editMode(let mode):
@@ -376,7 +376,7 @@ extension EncounterDetailViewState {
             Reducer.withState({ $0.building.id }) { state in
                 ResumableRunningEncounters.reducer { env in
                     do {
-                        let nodes = try env.database.keyValueStore.fetchAllRaw(RunningEncounter.keyPrefix(for: state.building))
+                        let nodes = try env.database.keyValueStore.fetchAllRaw(RunningEncounter.keyPrefix(for: state.building).rawValue)
                         return Just(nodes).setFailureType(to: Error.self).eraseToAnyPublisher()
                     } catch {
                         return Fail(error: error).eraseToAnyPublisher()
@@ -390,7 +390,7 @@ extension EncounterDetailViewState {
 }
 
 extension EncounterDetailViewState: NavigationStackItemState {
-    var navigationStackItemStateId: String { encounter.key }
+    var navigationStackItemStateId: String { encounter.rawKey }
 
     var navigationTitle: String { encounter.name }
     var navigationTitleDisplayMode: NavigationBarItem.TitleDisplayMode? { .inline }

commit 9020cd96188082ed15efa8b871bebd2ac2e664fa

Fix RunningEncounter entity key
Was broken by 330ebb060bc00569c9aedcd8c8ffcbeae062d563

Sources/Persistence/GameModelsKeyValueEntities.swift
@@ -10,7 +10,7 @@ import GameModels
 import Tagged
 
 extension RunningEncounter: KeyValueStoreEntity {
-    public static var keyPrefix: String = "running"
+    public static var keyPrefix: String = "runningEncounter"
 
     public var key: Key {
         return Self.keyPrefix(for: base) + id.uuidString