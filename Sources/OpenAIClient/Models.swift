75c0ed3fbdcc0cc8a9c3f7902a84ced37bd5bd0f
Thomas Visser <thomas.visser@gmail.com>
2023-04-19 21:01:01 +0200

    Add GPT-4 to the Models enum

--- Sources/OpenAIClient/Models.swift
+++ Sources/OpenAIClient/Models.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 public enum Model: String, Codable {
+    case gpt4 = "gpt-4"
     case gpt35Turbo = "gpt-3.5-turbo"
     case Davinci3 = "text-davinci-003"
     case Curie1 = "text-curie-001"
